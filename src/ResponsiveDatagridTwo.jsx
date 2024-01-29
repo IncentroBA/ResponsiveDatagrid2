26f6fc803bcca874751f7d198e2729aa912fa8fe
iamrob <rob.duits@incentro.com>
2024-01-29 14:36:34 +0100

    Update widget
    Adapt to filtering

--- src/ResponsiveDatagridTwo.jsx
+++ src/ResponsiveDatagridTwo.jsx
@@ -103,7 +103,8 @@ export function ResponsiveDatagridTwo({
             // Check first TD if there is no chevron present
             if (!row.querySelector(".td.chevron-td")) {
                 const selectTD = row.querySelector(".td");
-                const extraTD = `<div class="td chevron-td btn-td-collapse" role="gridcell"><div class="td-custom-content"><svg height="16" width="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M8.00004 11.1369C8.21033 11.1369 8.38741 11.0539 8.54789 10.8934L12.4935 6.85368C12.6208 6.72087 12.6872 6.56592 12.6872 6.37777C12.6872 5.9904 12.3829 5.68604 12.0066 5.68604C11.8239 5.68604 11.6469 5.76351 11.5085 5.90186L8.00557 9.50439L4.49158 5.90186C4.35876 5.76904 4.18722 5.68604 3.99353 5.68604C3.61723 5.68604 3.31287 5.9904 3.31287 6.37777C3.31287 6.56038 3.38481 6.72087 3.51208 6.85368L7.45772 10.8934C7.62374 11.0594 7.79529 11.1369 8.00004 11.1369Z"/></svg></div></div>`;
+                const borderClass = row.querySelector(".td-borders") ? "td-borders " : "";
+                const extraTD = `<div class="td ${borderClass}chevron-td btn-td-collapse" role="gridcell"><div class="td-custom-content"><svg height="16" width="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M8.00004 11.1369C8.21033 11.1369 8.38741 11.0539 8.54789 10.8934L12.4935 6.85368C12.6208 6.72087 12.6872 6.56592 12.6872 6.37777C12.6872 5.9904 12.3829 5.68604 12.0066 5.68604C11.8239 5.68604 11.6469 5.76351 11.5085 5.90186L8.00557 9.50439L4.49158 5.90186C4.35876 5.76904 4.18722 5.68604 3.99353 5.68604C3.61723 5.68604 3.31287 5.9904 3.31287 6.37777C3.31287 6.56038 3.38481 6.72087 3.51208 6.85368L7.45772 10.8934C7.62374 11.0594 7.79529 11.1369 8.00004 11.1369Z"/></svg></div></div>`;
                 if (selectTD && selectTD.querySelector(".td-custom-content")) {
                     selectTD.insertAdjacentHTML("afterend", extraTD);
                 } else if (selectTD) {
@@ -123,15 +124,16 @@ export function ResponsiveDatagridTwo({
             const endsWithCustomContent = checkLastColumn(row);
             const offset = endsWithCustomContent ? -1 : 0;
             const visibleColumns = screenMode === "mobile" ? maxColumnsMobile : maxColumnsTablet;
-
             const THs = [...row.querySelectorAll(".th")];
             if (THs.length) {
-                THs.slice(visibleColumns, offset).forEach(TH => TH.classList.add("hidden"));
+                THs.slice(THs.length - (THs.length - visibleColumns) - offset).forEach(TH => {
+                    TH.classList.add("hidden");
+                });
             }
 
             const TDs = [...row.querySelectorAll(".td")];
             if (TDs.length) {
-                const newTDs = TDs.slice(visibleColumns, offset);
+                const newTDs = TDs.slice(TDs.length - (TDs.length - visibleColumns) - offset);
                 newTDs.forEach((TD, index) => {
                     TD.classList.add("hidden");
                     const headers = [
@@ -141,12 +143,27 @@ export function ResponsiveDatagridTwo({
                     ];
                     const collapsible = row.querySelector(".tr-collapsible");
                     const collapsibleContent = `<div class="tr-collapsible__item">
-          <p class="td-text text-bold nomargin">${headers[index]?.querySelector("span").innerHTML}</p>
-          <span class="td-text">${TD.querySelector("span")?.innerHTML}</span>
-        </div>`;
+                        <p class="td-text text-bold nomargin">${headers[index]?.querySelector("span").innerHTML}</p>
+                            <span class="td-text">${TD.querySelector("span")?.innerHTML}</span>
+                        </div>`;
                     collapsible.insertAdjacentHTML("beforeend", collapsibleContent);
                 });
             }
+        }
+
+        function renderElements() {
+            // Process each row
+            const rows = [...datagridWidgetRef.current.querySelectorAll(".tr[role=row]")];
+            rows.forEach(row => {
+                renderCollapsibleDiv(row);
+                moveColumnsInsideCollapsibleDiv(row);
+                renderChevron(row);
+            });
+
+            const chevronBtns = [...datagridWidgetRef.current.querySelectorAll(".btn-td-collapse")];
+            chevronBtns.forEach(chevronBtn =>
+                chevronBtn.addEventListener("click", event => toggleTrCollapse(event, chevronBtn))
+            );
         }
 
         function resetCollapsibles() {
@@ -165,6 +182,21 @@ export function ResponsiveDatagridTwo({
                 row.querySelector(".th.chevron-th")?.remove();
                 row.querySelector(".td.chevron-td")?.remove();
             });
+        }
+
+        function onSort() {
+            setTimeout(() => {
+                resetCollapsibles();
+                resetHiddenColumns();
+                resetChevrons();
+                renderElements();
+            }, 100);
+        }
+
+        function checkForSorting() {
+            const rows = [...datagridWidgetRef.current.querySelectorAll(".tr[role=row]")];
+            const headers = [...rows[0].querySelectorAll(".th")];
+            headers.forEach(header => header.addEventListener("click", onSort));
         }
 
         if (canRender && screenMode) {
@@ -197,18 +229,8 @@ export function ResponsiveDatagridTwo({
                 resetHiddenColumns();
                 resetChevrons();
 
-                // Process each row
-                const rows = [...datagridWidgetRef.current.querySelectorAll('.tr[role="row"]')];
-                rows.forEach(row => {
-                    renderChevron(row);
-                    renderCollapsibleDiv(row);
-                    moveColumnsInsideCollapsibleDiv(row);
-                });
-
-                const chevronBtns = [...datagridWidgetRef.current.querySelectorAll(".btn-td-collapse")];
-                chevronBtns.forEach(chevronBtn =>
-                    chevronBtn.addEventListener("click", event => toggleTrCollapse(event, chevronBtn))
-                );
+                renderElements();
+                checkForSorting();
             }
         }
     });
