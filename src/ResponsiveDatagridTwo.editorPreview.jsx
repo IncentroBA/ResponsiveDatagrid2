461a5c290d0eea3c8c1cb4669c21783f53061f34
iamrob <rob.duits@incentro.com>
2024-01-29 15:11:53 +0100

    Update editor preview

--- src/ResponsiveDatagridTwo.editorPreview.jsx
+++ src/ResponsiveDatagridTwo.editorPreview.jsx
@@ -1,7 +1,11 @@
 import { createElement } from "react";
 
 export function preview() {
-    return <div></div>;
+    return (
+        <div className="responsive-datagrid-preview">
+            Responsive Data grid 2 widget (does not display the data grid that is inside)
+        </div>
+    );
 }
 
 export function getPreviewCss() {
