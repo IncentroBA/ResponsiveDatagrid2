3e0156a9666e5be4ce662b9d6cad739962aa21a7
iamrob <rob.duits@incentro.com>
2024-07-04 09:12:48 +0200

    Add options
    RenderChevron (left | right) option
    forceAutoFill (boolean) optional option to render at least 1 autofill column

--- src/ResponsiveDatagridTwo.jsx
+++ src/ResponsiveDatagridTwo.jsx
@@ -7,7 +7,9 @@ export function ResponsiveDatagridTwo({
     keepLastBoolean,
     maxColumnsMobile,
     maxColumnsTablet,
+    forceAutoFill,
     mobileBreakpoint,
+    RenderChevron,
     ...rest
 }) {
     const style = rest.class || "";
@@ -42,17 +44,28 @@ export function ResponsiveDatagridTwo({
 
     useEffect(() => {
         function setGridColumns() {
-            const offset = keepLastBoolean ? -1 : 0;
-            const columnArray = templateColumns.replace("grid-template-columns: ", "").split(/ (?![^()]*\))/); // Regex for space when not inside ()
+            const columnArray = templateColumns.replace("grid-template-columns: ", "").split(/ (?![^()]*\))/);
             const visibleColumns = screenMode === "mobile" ? maxColumnsMobile : maxColumnsTablet;
-            const maxColumnArray = columnArray.slice(columnArray.length - visibleColumns - offset); // remove all but the last maxColumnsMobile
+            const maxColumnArray = columnArray.slice(
+                0,
+                columnArray.length - (columnArray.length - visibleColumns) + (keepLastBoolean ? -1 : 0)
+            );
 
-            if (offset) {
+            if (keepLastBoolean) {
                 maxColumnArray.push("fit-content(100%)");
             }
             maxColumnArray.join(" ").replace(";", "");
-            maxColumnArray.unshift("fit-content(100%)"); // add new column for the chevron
-            maxColumnArray.splice(2, 1, "1fr"); // convert at least one auto-fill column to auto-fill
+
+            if (RenderChevron === "right") {
+                maxColumnArray.push("fit-content(100%)");
+            } else {
+                maxColumnArray.unshift("fit-content(100%)");
+            }
+
+            if (forceAutoFill) {
+                maxColumnArray.splice(2, 1, "1fr");
+            }
+
             tableContent.setAttribute("style", `grid-template-columns: ${maxColumnArray.join(" ").replace(";", "")};`);
         }
 
@@ -81,11 +94,15 @@ export function ResponsiveDatagridTwo({
 
         // Check the column to insert the chevron
         function renderChevron(row) {
+            const visibleColumns = screenMode === "mobile" ? maxColumnsMobile : maxColumnsTablet;
             // Check first TH if there is no chevron present
             if (!row.querySelector(".th.chevron-th")) {
-                const selectTH = row.querySelector(".th");
+                const selectTH =
+                    RenderChevron === "left"
+                        ? row.querySelector(".th")
+                        : row.querySelector(`.th:nth-child(${visibleColumns + 1})`);
                 const extraTH = `<div class="th chevron-th" role="columnheader"><div class="column-container">&nbsp;</div></div>`;
-                if (selectTH && selectTH.querySelector(".td-custom-content")) {
+                if (selectTH && selectTH.querySelector(".td-custom-content") && RenderChevron === "left") {
                     selectTH.insertAdjacentHTML("afterend", extraTH);
                 } else if (selectTH) {
                     selectTH.insertAdjacentHTML("beforebegin", extraTH);
@@ -94,10 +111,13 @@ export function ResponsiveDatagridTwo({
 
             // Check first TD if there is no chevron present
             if (!row.querySelector(".td.chevron-td")) {
-                const selectTD = row.querySelector(".td");
+                const selectTD =
+                    RenderChevron === "left"
+                        ? row.querySelector(".td")
+                        : row.querySelector(`.td:nth-child(${visibleColumns + 1})`);
                 const borderClass = row.querySelector(".td-borders") ? "td-borders " : "";
                 const extraTD = `<div class="td ${borderClass}chevron-td btn-td-resp-collapse" role="gridcell"><div class="td-custom-content"><svg height="16" width="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M8.00004 11.1369C8.21033 11.1369 8.38741 11.0539 8.54789 10.8934L12.4935 6.85368C12.6208 6.72087 12.6872 6.56592 12.6872 6.37777C12.6872 5.9904 12.3829 5.68604 12.0066 5.68604C11.8239 5.68604 11.6469 5.76351 11.5085 5.90186L8.00557 9.50439L4.49158 5.90186C4.35876 5.76904 4.18722 5.68604 3.99353 5.68604C3.61723 5.68604 3.31287 5.9904 3.31287 6.37777C3.31287 6.56038 3.38481 6.72087 3.51208 6.85368L7.45772 10.8934C7.62374 11.0594 7.79529 11.1369 8.00004 11.1369Z"/></svg></div></div>`;
-                if (selectTD && selectTD.querySelector(".td-custom-content")) {
+                if (selectTD && selectTD.querySelector(".td-custom-content") && RenderChevron === "left") {
                     selectTD.insertAdjacentHTML("afterend", extraTD);
                 } else if (selectTD) {
                     selectTD.insertAdjacentHTML("beforebegin", extraTD);
@@ -113,31 +133,59 @@ export function ResponsiveDatagridTwo({
         }
 
         function moveColumnsInsideCollapsibleDiv(row) {
-            const offset = keepLastBoolean ? -1 : 0;
             const visibleColumns = screenMode === "mobile" ? maxColumnsMobile : maxColumnsTablet;
+            const endIndex = keepLastBoolean ? -1 : 0;
             const THs = [...row.querySelectorAll(".th")];
+
             if (THs.length) {
-                THs.slice(THs.length - (THs.length - visibleColumns) + offset, offset).forEach(TH => {
-                    TH.classList.add("hidden");
-                });
+                const startIndex = THs.length - (THs.length - visibleColumns) + (keepLastBoolean ? -1 : 0);
+
+                if (keepLastBoolean) {
+                    THs.slice(startIndex, endIndex).forEach(TH => {
+                        TH.classList.add("hidden");
+                    });
+                } else {
+                    THs.slice(startIndex).forEach(TH => {
+                        TH.classList.add("hidden");
+                    });
+                }
             }
 
             const TDs = [...row.querySelectorAll(".td")];
             if (TDs.length) {
-                TDs.slice(TDs.length - (TDs.length - visibleColumns) + offset, offset).forEach((TD, index) => {
-                    TD.classList.add("hidden");
-                    const headers = [
-                        ...datagridWidgetRef.current.querySelectorAll(
-                            '.widget-datagrid .tr[role="row"]:first-child .th.hidden'
-                        )
-                    ];
-                    const collapsible = row.querySelector(".tr-resp-collapsible");
-                    const collapsibleContent = `<div class="tr-resp-collapsible__item">
+                const startIndex = TDs.length - (TDs.length - visibleColumns) + (keepLastBoolean ? -1 : 0);
+
+                if (keepLastBoolean) {
+                    TDs.slice(startIndex, endIndex).forEach((TD, index) => {
+                        TD.classList.add("hidden");
+                        const headers = [
+                            ...datagridWidgetRef.current.querySelectorAll(
+                                '.widget-datagrid .tr[role="row"]:first-child .th.hidden'
+                            )
+                        ];
+                        const collapsible = row.querySelector(".tr-resp-collapsible");
+                        const collapsibleContent = `<div class="tr-resp-collapsible__item">
+                            <p class="td-text text-bold nomargin">${headers[index]?.querySelector("span").innerHTML}</p>
+                                <span class="td-text">${TD.querySelector("span")?.innerHTML}</span>
+                            </div>`;
+                        collapsible.insertAdjacentHTML("beforeend", collapsibleContent);
+                    });
+                } else {
+                    TDs.slice(startIndex).forEach((TD, index) => {
+                        TD.classList.add("hidden");
+                        const headers = [
+                            ...datagridWidgetRef.current.querySelectorAll(
+                                '.widget-datagrid .tr[role="row"]:first-child .th.hidden'
+                            )
+                        ];
+                        const collapsible = row.querySelector(".tr-resp-collapsible");
+                        const collapsibleContent = `<div class="tr-resp-collapsible__item">
                         <p class="td-text text-bold nomargin">${headers[index]?.querySelector("span").innerHTML}</p>
                             <span class="td-text">${TD.querySelector("span")?.innerHTML}</span>
                         </div>`;
-                    collapsible.insertAdjacentHTML("beforeend", collapsibleContent);
-                });
+                        collapsible.insertAdjacentHTML("beforeend", collapsibleContent);
+                    });
+                }
             }
         }
 
